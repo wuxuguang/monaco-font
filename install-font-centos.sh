686ae1bba20c08b7f2d32a7ab3a767672c4af29f
wuxuguang <buptwxg@163.com>
2015-03-24 18:54:33 +0800

    add font file,install shell script

---
+++ install-font-centos.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Start install"
+mkdir -p /usr/share/fonts/monaco
+
+echo "Entering Font Directory"
+cd /usr/share/fonts/monaco
+
+echo "Downloading font"
+wget -c https://github.com/cstrap/monaco-font/raw/master/Monaco_Linux.ttf
+
+echo "Installing font"
+chmod 644 Monaco_Linux.ttf
+mkfontscale
+mkfontdir
+
+echo "Leaving Font Directory"
+cd ~
+
+echo "Enjoy"
